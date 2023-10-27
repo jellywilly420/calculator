69f59005a28156e0968316dfe11d1428ab63b926
jellywilly420 <65194730+jellywilly420@users.noreply.github.com>
2023-10-27 14:50:54 +0200

    Add basic mathematical funtions Install node

---
+++ script.js
@@ -0,0 +1,17 @@
+function add(operand1, operand2){
+    return operand1+operand2;
+}
+function substract(operand1, operand2){
+    return operand1-operand2;
+}
+function multiply(operand1, operand2){
+    return (operand1*operand2);
+}
+function divide(operand1, operand2){
+    return (operand1/operand2);
+}
+function power(operand, power){
+    return (operand**power)
+}
+
+// console.log(power(8,-1))
